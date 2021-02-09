c01cb66633018fa54edb10fb7c25278a9574d830
matt <matthewk.rydalch@gmail.com>
2021-02-09 16:05:01 -0700

    Trying to get the pid class to hold steady like before.

--- scripts/offboard_velocity_ned.py
+++ scripts/offboard_velocity_ned.py
@@ -12,6 +12,7 @@ from pid_class import PID
 
 class VelCntrl:
     def __init__(self):
+        self.prev_time = 0.0 #seconds
         self.ned_desired = [0.0,0.0,-2.0]
 
         kpN = 1.0
@@ -36,44 +37,78 @@ class VelCntrl:
         self.downPid = PID(kpD,kiD,kdD,tauD,maxDDot)
 
         self.ref_set = False
+        self.time_set = False
 
     async def run(self):
         """ Does Offboard control using velocity NED coordinates. """
 
-        drone = System()
-        await drone.connect(system_address="udp://:14540")
+        self.drone = System()
+        await self.drone.connect(system_address="udp://:14540")
 
         print("Waiting for drone to connect...")
-        async for state in drone.core.connection_state():
+        async for state in self.drone.core.connection_state():
             if state.is_connected:
                 print(f"Drone discovered with UUID: {state.uuid}")
                 break
 
         print("-- Arming")
-        await drone.action.arm()
+        await self.drone.action.arm()
 
         print("-- Setting initial setpoint")
-        await drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
+        await self.drone.offboard.set_velocity_ned(VelocityNedYaw(0.0, 0.0, 0.0, 0.0))
 
         print("-- Starting offboard")
         try:
-            await drone.offboard.start()
+            await self.drone.offboard.start()
         except OffboardError as error:
             print(f"Starting offboard mode failed with error code: \
                 {error._result.result}")
             print("-- Disarming")
-            await drone.action.disarm()
+            await self.drone.action.disarm()
             return
 
-        async for lla in drone.telemetry.position():
-            if self.ref_set:
-                cmdVel = self.get_commands(lla)
-                await drone.offboard.set_velocity_ned(VelocityNedYaw(cmdVel[0],cmdVel[1],cmdVel[2], 0.0))
+        # secondary_task = asyncio.ensure_future(self.get_time())
+
+        async for lla in self.drone.telemetry.position():
+            async for odom in self.drone.telemetry.odometry():
+                if self.time_set:
+                    self.time = odom.time_usec*1000000
+                    break
+                else:
+                    self.prev_time = odom.time_usec*1000000
+                    self.time_set = True
+            if self.ref_set and self.time_set:
+                cmdVel = self.get_commands(lla,self.time)
+                print('cmdVel = ', cmdVel)
+                await self.drone.offboard.set_velocity_ned(VelocityNedYaw(cmdVel[0],cmdVel[1],cmdVel[2], 0.0))
             else:
                 self.lat_ref = lla.latitude_deg
                 self.lon_ref = lla.longitude_deg
                 self.alt_ref = lla.absolute_altitude_m
                 self.ref_set = True
+
+        # await secondary_task
+
+    # async def get_time(self):
+    #     async for odom in self.drone.telemetry.odometry():
+    #         if self.time_set:
+    #             self.time = odom.time_usec*1000000
+    #         else:
+    #             self.prev_time = odom.time_usec*1000000
+    #             self.time_set = True
+    
+    # async def vel_cmds(self):
+    #     async for lla in self.drone.telemetry.position():
+    #         print('outside of command routine')
+    #         if self.ref_set and self.time_set:
+    #             print('in command routine')
+    #             cmdVel = self.get_commands(lla,self.time)
+    #             await self.drone.offboard.set_velocity_ned(VelocityNedYaw(cmdVel[0],cmdVel[1],cmdVel[2], 0.0))
+    #         else:
+    #             self.lat_ref = lla.latitude_deg
+    #             self.lon_ref = lla.longitude_deg
+    #             self.alt_ref = lla.absolute_altitude_m
+    #             self.ref_set = True
 
         # await asyncio.sleep(50)
 
@@ -113,15 +148,21 @@ class VelCntrl:
         #     print(f"Stopping offboard mode failed with error code: \
         #         {error._result.result}")
 
-    def get_commands(self,lla):
+    def get_commands(self,lla,time):
         ned = navpy.lla2ned(lla.latitude_deg,lla.longitude_deg,lla.absolute_altitude_m,self.lat_ref,self.lon_ref,self.alt_ref)
-        
-        self.northPid.update_control(ned[0],self.ned_desired[0],dt)
-        cmdX = self.northPid.command
-        self.eastPid.update_control(ned[1],self.ned_desired[1],dt)
-        cmdY = self.eastPid.command
-        self.downPid.update_control(ned[2],self.ned_desired[2],dt)
-        cmdZ = self.downPid.command
+        dt = time - self.prev_time
+
+        cmdX = self.ned_desired[0] - ned[0]
+        cmdY = self.ned_desired[1] - ned[1]
+        cmdZ = self.ned_desired[2] - ned[2]
+        # self.northPid.update_control(ned[0],self.ned_desired[0],dt)
+        # cmdX = self.northPid.command
+        # self.eastPid.update_control(ned[1],self.ned_desired[1],dt)
+        # cmdY = self.eastPid.command
+        # self.downPid.update_control(ned[2],self.ned_desired[2],dt)
+        # cmdZ = self.downPid.command
+
+        self.prev_time = time
 
         cmd = [cmdX,cmdY,cmdZ]
         return cmd
--- scripts/pid_class.py
+++ scripts/pid_class.py
@@ -14,37 +14,42 @@ class PID:
         self.command = 0.0
 
     def update_control(self,state,desired,dt):
-        error = state-desired
-        self.update_integrator(error,dt)
-        self.update_derivative(state,dt)
+        # if dt < 0.0001:
+        #     return
+        error = desired-state
+        # if self.ki != 0.0:
+        #     self.update_integrator(error,dt)
+        # if self.kd != 0.0:
+        #     self.update_derivative(state,dt)
         self.update_command(error)
-        self.prev_state = state
+        # self.prev_state = state
 
-    def update_integrator(self,error,dt):
-        self.integrator = self.integrator+error*dt
+    # def update_integrator(self,error,dt):
+    #     self.integrator = self.integrator+error*dt
 
-    def update_derivative(self,state,dt):
-        dState = state - self.prev_state
-        self.derivative = (2.0*self.tau-dt)*self.derivative + (2.0*dState)/(2.0*self.tau + dt)
+    # def update_derivative(self,state,dt):
+    #     dState = state - self.prev_state
+    #     self.derivative = (2.0*self.tau-dt)*self.derivative + (2.0*dState)/(2.0*self.tau + dt)
 
     def update_command(self,error):
         pTerm = self.kp*error
-        iTerm = self.ki*self.integrator
-        dTerm = self.kd*self.derivative
-        u = pTerm+iTerm-dTerm
-        u_sat = self.saturate(u)
-        self.compute_anti_windup(u,u_sat,pTerm,iTerm,dTerm)
-        self.command = u_sat
+        self.command = pTerm
+        # iTerm = self.ki*self.integrator
+        # dTerm = self.kd*self.derivative
+        # u = pTerm+iTerm-dTerm
+        # u_sat = self.saturate(u)
+        # self.compute_anti_windup(u,u_sat,pTerm,iTerm,dTerm)
+        # self.command = u_sat
 
-    def saturate(self,u):
-        if u > self.maxDot:
-            u_sat = self.maxDot
-        elif u < -self.maxDot:
-            u_sat = -self.maxDot
-        else:
-            u_sat = u
-        return u_sat
+    # def saturate(self,u):
+    #     if u > self.maxDot:
+    #         u_sat = self.maxDot
+    #     elif u < -self.maxDot:
+    #         u_sat = -self.maxDot
+    #     else:
+    #         u_sat = u
+    #     return u_sat
 
-    def compute_anti_windup(self,u,u_sat,pTerm,iTerm,dTerm):
-        if (u != u_sat and abs(iTerm)>abs(u_sat-pTerm+dTerm)):
-            self.integrator = (u_sat-pTerm+dTerm)/self.ki
+    # def compute_anti_windup(self,u,u_sat,pTerm,iTerm,dTerm):
+    #     if (u != u_sat and abs(iTerm)>abs(u_sat-pTerm+dTerm)):
+    #         self.integrator = (u_sat-pTerm+dTerm)/self.ki
