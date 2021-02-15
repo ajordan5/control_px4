c850d25f9897a8d306fdc2ce04edbe3ce03e1023
matt <matthewk.rydalch@gmail.com>
2021-02-15 11:41:37 -0700

    Publishing ublox messages now.

--- scripts/pose2ublox_ros.py
+++ scripts/pose2ublox_ros.py
@@ -6,9 +6,8 @@ import rospy
 from pose2ublox import Pose2Ublox
 
 from geometry_msgs.msg import PoseStamped
-# from ublox.msg import PosVelEcef
-# from ublox.msg import RelPos
-# from rosflight_msgs.msg import GNSS
+from ublox.msg import PosVelEcef
+from ublox.msg import RelPos
 
 class Pose2Ublox_Ros():
     def __init__(self):
@@ -21,10 +20,10 @@ class Pose2Ublox_Ros():
             self.sigma_base_vel, self.lpf_on, self.A, self.B)
 
         # Publishers
-        # self.rover_virtual_relpos_pub_ = rospy.Publisher('RelPos', RelPos, queue_size=5, latch=True)
-        # self.rover_virtual_PosVelEcef_pub_ = rospy.Publisher('PosVelEcef', PosVelEcef, queue_size=5, latch=True)
-        # self.compass_virtual_relPos_pub_ = rospy.Publisher('boat/compass/RelPos', RelPos, queue_size=5, latch=True)
-        # self.base_virtual_PosVelEcef_pub_ = rospy.Publisher('boat/PosVelEcef', PosVelEcef, queue_size=5, latch=True)
+        self.rover_virtual_relpos_pub_ = rospy.Publisher('RelPos', RelPos, queue_size=5, latch=True)
+        self.rover_virtual_PosVelEcef_pub_ = rospy.Publisher('PosVelEcef', PosVelEcef, queue_size=5, latch=True)
+        self.compass_virtual_relPos_pub_ = rospy.Publisher('boat/compass/RelPos', RelPos, queue_size=5, latch=True)
+        self.base_virtual_PosVelEcef_pub_ = rospy.Publisher('boat/PosVelEcef', PosVelEcef, queue_size=5, latch=True)
         
         # Timer
         self.ublox_rate_timer_ = rospy.Timer(rospy.Duration(self.Ts), self.ubloxRateCallback)
@@ -48,51 +47,53 @@ class Pose2Ublox_Ros():
         self.m2u.update_base_virtual_PosVelEcef(dt)
 
         #publish messages
-        # self.publish_rover_virtual_PosVelEcef(time_stamp)
-        # self.publish_rover_virtual_relPos()
-        # self.publish_base_virtual_PosVelEcef(time_stamp)
-        # self.publish_compass_virtual_relPos()
+        self.publish_rover_virtual_PosVelEcef(time_stamp)
+        self.publish_rover_virtual_relPos()
+        self.publish_base_virtual_PosVelEcef(time_stamp)
+        self.publish_compass_virtual_relPos()
 
-    # def publish_rover_virtual_PosVelEcef(self, time_stamp):
+    def publish_rover_virtual_PosVelEcef(self, time_stamp):
 
-    #     self.rover_PosVelEcef.header.stamp = time_stamp
-    #     self.rover_PosVelEcef.fix = 3
-    #     # # self.rover_PosVelEcef.lla = self.rover_lla  #lla is not currently being used            
-    #     self.rover_PosVelEcef.position = self.m2u.rover_virtual_pos_ecef
-    #     self.rover_PosVelEcef.horizontal_accuracy = self.global_horizontal_accuracy
-    #     self.rover_PosVelEcef.vertical_accuracy = self.global_vertical_accuracy
-    #     self.rover_PosVelEcef.velocity = self.m2u.rover_virtual_vel_ecef
-    #     self.rover_PosVelEcef.speed_accuracy = self.global_speed_accuracy
+        self.rover_PosVelEcef.header.stamp = time_stamp
+        self.rover_PosVelEcef.fix = 3
+        # # self.rover_PosVelEcef.lla = self.rover_lla  #lla is not currently being used            
+        self.rover_PosVelEcef.position = self.m2u.rover_virtual_pos_ecef
+        self.rover_PosVelEcef.horizontal_accuracy = self.global_horizontal_accuracy
+        self.rover_PosVelEcef.vertical_accuracy = self.global_vertical_accuracy
+        self.rover_PosVelEcef.velocity = self.m2u.rover_virtual_vel_ecef
+        self.rover_PosVelEcef.speed_accuracy = self.global_speed_accuracy
 
-    #     self.rover_virtual_PosVelEcef_pub_.publish(self.rover_PosVelEcef)
+        self.rover_virtual_PosVelEcef_pub_.publish(self.rover_PosVelEcef)
 
 
-    # def publish_rover_virtual_relPos(self):
+    def publish_rover_virtual_relPos(self):
 
-    #     self.rover_relPos.header.stamp = rospy.Time.now()
-    #     self.rover_relPos.relPosNED[0] = self.m2u.rover_virtual_relpos[0]
-    #     self.rover_relPos.relPosNED[1] = self.m2u.rover_virtual_relpos[1]
-    #     self.rover_relPos.relPosNED[2] = self.m2u.rover_virtual_relpos[2]
+        self.rover_relPos.header.stamp = rospy.Time.now()
+        self.rover_relPos.relPosNED[0] = self.m2u.rover_virtual_relpos[0]
+        self.rover_relPos.relPosNED[1] = self.m2u.rover_virtual_relpos[1]
+        self.rover_relPos.relPosNED[2] = self.m2u.rover_virtual_relpos[2]
 
-    #     self.rover_virtual_relpos_pub_.publish(self.rover_relPos)
+        self.rover_virtual_relpos_pub_.publish(self.rover_relPos)
 
-    # def publish_base_virtual_PosVelEcef(self, time_stamp):
+    def publish_base_virtual_PosVelEcef(self, time_stamp):
 
-    #     self.base_PosVelEcef.header.stamp = time_stamp        print(self.prev_time)
-# rtual_pos_ecef
-    #     self.base_PosVelEcef.horizontal_accuracy = self.global_horizontal_accuracy
-    #     self.base_PosVelEcef.vertical_accuracy = self.global_vertical_accuracy
-    #     self.base_PosVelEcef.velocity = self.m2u.base_virtual_vel_ecef
-    #     self.base_PosVelEcef.speed_accuracy = self.global_speed_accuracy
+        self.base_PosVelEcef.header.stamp = time_stamp
+        self.base_PosVelEcef.fix = 3
+        # # self.base_PosVelEcef.lla = self.base_lla  #lla is not currently being used            
+        self.base_PosVelEcef.position = self.m2u.base_virtual_pos_ecef
+        self.base_PosVelEcef.horizontal_accuracy = self.global_horizontal_accuracy
+        self.base_PosVelEcef.vertical_accuracy = self.global_vertical_accuracy
+        self.base_PosVelEcef.velocity = self.m2u.base_virtual_vel_ecef
+        self.base_PosVelEcef.speed_accuracy = self.global_speed_accuracy
 
-    #     self.base_virtual_PosVelEcef_pub_.publish(self.base_PosVelEcef)
+        self.base_virtual_PosVelEcef_pub_.publish(self.base_PosVelEcef)
 
     
-    # def publish_compass_virtual_relPos(self):
+    def publish_compass_virtual_relPos(self):
 
-    #     self.compass_relPos.relPosHeading = self.m2u.compass_heading
-    #     self.compass_relPos.accHeading = self.accHeading
-    #     self.compass_virtual_relPos_pub_.publish(self.compass_relPos)
+        self.compass_relPos.relPosHeading = self.m2u.compass_heading
+        self.compass_relPos.accHeading = self.accHeading
+        self.compass_virtual_relPos_pub_.publish(self.compass_relPos)
 
     
     def load_set_parameters(self):
@@ -119,30 +120,10 @@ class Pose2Ublox_Ros():
         self.B = rospy.get_param('~B', 6356752.314245)
 
         #message types
-        # self.rover_PosVelEcef = PosVelEcef()
-        # self.rover_relPos = RelPos()
-        # self.compass_relPos = RelPos()
-        # self.base_PosVelEcef = PosVelEcef()
+        self.rover_PosVelEcef = PosVelEcef()
+        self.rover_relPos = RelPos()
+        self.compass_relPos = RelPos()
+        self.base_PosVelEcef = PosVelEcef()
 
         #used for updating dt
         self.prev_time = 0.0
-
-        ########################
-        print(self.Ts)
-        print(self.global_horizontal_accuracy)
-        print(self.global_vertical_accuracy)
-        print(self.global_speed_accuracy)
-        print(self.relative_horizontal_accuracy)
-        print(self.relative_vertical_accuracy)
-        print(self.relative_speed_accuracy)
-        print(self.accHeading) #The noise is currently not applied to the heading.
-        print(self.noise_on)
-        print(self.ref_lla)
-        print(self.sigma_rover_pos) 
-        print(self.sigma_rover_vel)
-        print(self.sigma_rover_relpos)
-        print(self.sigma_base_pos)
-        print(self.sigma_base_vel)
-        print(self.lpf_on)
-        print(self.A)
-        print(self.B)
