9cf8d34abf681f5d14e850f4041f9aaaf2f85005
matt <matthewk.rydalch@gmail.com>
2021-02-17 07:58:13 -0700

    Modified mocap2ublox with changes I made with sim.

--- scripts/pose2ublox/mocap2ublox.py
+++ scripts/pose2ublox/mocap2ublox.py
@@ -7,6 +7,8 @@ from geometry_msgs.msg import PoseStamped
 class Mocap2Ublox(Pose2Ublox_Ros):
     def __init__(self):
         super().__init__()
+        self.receivedRoverPose = False
+        self.receivedBasePose = False
         # Subscribers
         self.rover_mocap_ned_sub_ = rospy.Subscriber('rover_pose', PoseStamped, self.roverNedCallback, queue_size=5)
         self.base_mocap_ned_sub_ = rospy.Subscriber('base_mocap', PoseStamped, self.baseNedCallback, queue_size=5)
@@ -16,27 +18,33 @@ class Mocap2Ublox(Pose2Ublox_Ros):
             rospy.spin()
 
     def roverNedCallback(self, msg):
-        
-        self.m2u.rover_ned = np.array([msg.pose.position.x,
+        self.p2u.rover_ned = np.array([msg.pose.position.x,
                                    msg.pose.position.y,
                                    msg.pose.position.z])
-
+        if not self.receivedPose:
+            self.receivedRoverPose = True
+            if self.receivedBasePose:
+                self.receivedPose = True
 
     def baseNedCallback(self, msg):
 
-        self.m2u.base_ned = np.array([msg.pose.position.x,
+        self.p2u.base_ned = np.array([msg.pose.position.x,
                                    msg.pose.position.y,
                                    msg.pose.position.z])
 
-        self.m2u.compass_quat = np.array([msg.pose.orientation.w,
+        self.p2u.compass_quat = np.array([msg.pose.orientation.w,
                                         msg.pose.orientation.x,
                                         msg.pose.orientation.y,
                                         msg.pose.orientation.z])
         
+        if not self.receivedPose:
+            self.receivedBasePose = True
+            if self.receivedRoverPose:
+                self.receivedPose = True
 if __name__ == '__main__':
-    rospy.init_node('m2u', anonymous=True)
+    rospy.init_node('p2u', anonymous=True)
     try:
-        m2u = Mocap2Ublox()
+        p2u = Mocap2Ublox()
     except:
         rospy.ROSInterruptException
     pass
