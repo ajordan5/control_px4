bd235434f034ab3752f33c5934042035d313ca61
matt <matthewk.rydalch@gmail.com>
2021-03-03 13:33:46 -0700

    Added heading updates from mocap if properly mapped.

--- scripts/nav.py
+++ scripts/nav.py
@@ -15,6 +15,13 @@ class Nav:
         self.relPos = Point()
         self.pose_update = PoseWithCovarianceStamped()
 
+        self.roll_covariance = 1000000 #essentially infinite
+        self.pitch_covariance = 1000000
+        self.yaw_covariance = 1000000 
+        self.mocap_covariance = 0.0001
+        self.pose_update.header = msg.header
+        self.orientation[0] = [0.0,0.0,0.0,1.0]
+
         self.refLlaSet = False
 
         #TODO we may need to speed up command inputs in order to have good performance.
@@ -28,6 +35,7 @@ class Nav:
         self.rover_relPos_sub_ = rospy.Subscriber('rover_relPos', RelPos, self.roverRelPosCallback, queue_size=5)
         self.posVelEcef_sub_ = rospy.Subscriber('posVelEcef', PosVelEcef, self.posVelEcefCallback, queue_size=5)
         self.base_posVelEcef_sub_ = rospy.Subscriber('base_posVelEcef', PosVelEcef, self.basePosVelEcefCallback, queue_size=5)
+        self.rover_pose_4_heading_sub_ = rospy.Subscriber('rover_pose_4_heading', PoseStamped, self.roverPose4HeadingCallback, queue_size=5)
         # self.px4_estimate_sub_ = rospy.Subscriber('/px4_estimate', PoseStamped, self.px4EstimateCallback, queue_size=5)
 
         while not rospy.is_shutdown():
@@ -62,19 +70,28 @@ class Nav:
         covariance[0] = msg.horizontal_accuracy
         covariance[7] = msg.horizontal_accuracy
         covariance[14] = msg.vertical_accuracy
-        covariance[21] = 1000000 #essentially infinite
-        covariance[28] = 1000000
-        covariance[35] = 1000000 
+        covariance[21] = self.roll_covariance
+        covariance[28] = self.pitch_covariance
+        covariance[35] = self.yaw_covariance
         self.pose_update.header = msg.header
         self.pose_update.pose.pose.position.x = ned[0]
         self.pose_update.pose.pose.position.y = ned[1]
         self.pose_update.pose.pose.position.z = ned[2]
-        self.pose_update.pose.pose.orientation.x = 0.0
-        self.pose_update.pose.pose.orientation.y = 0.0
-        self.pose_update.pose.pose.orientation.z = 0.0
-        self.pose_update.pose.pose.orientation.w = 1.0
+        self.pose_update.pose.pose.orientation.x = self.orientation[0]
+        self.pose_update.pose.pose.orientation.y = self.orientation[1]
+        self.pose_update.pose.pose.orientation.z = self.orientation[2]
+        self.pose_update.pose.pose.orientation.w = self.orientation[3]
         self.pose_update.pose.covariance = covariance
         self.pose_update_pub_.publish(self.pose_update)
+
+    def roverPose4HeadingCallback(self,msg):
+        self.orientation[0] = msg.pose.orientation.x
+        self.orientation[1] = msg.pose.orientation.y
+        self.orientation[2] = msg.pose.orientation.z
+        self.orientation[3] = msg.pose.orientation.w
+        # self.roll_covariance = self.mocap_covariance #May need to uncomment these.  Not sure how the covariance is used.
+        # self.pitch_covariance = self.mocap_covariance
+        self.yaw_covariance = self.mocap_covariance
 
 if __name__ == '__main__':
     rospy.init_node('nav', anonymous=True)
