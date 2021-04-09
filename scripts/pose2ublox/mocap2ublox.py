Fixed a bug in mocap2ublox quaternion.
Also set up launch files.

scripts/pose2ublox/mocap2ublox.py
@@ -32,10 +32,10 @@ class Mocap2Ublox(Pose2Ublox_Ros):
                                    msg.pose.position.y,
                                    msg.pose.position.z])
 
-        self.p2u.compass_quat = np.array([msg.pose.orientation.w,
-                                        msg.pose.orientation.x,
+        self.p2u.compass_quat = np.array([msg.pose.orientation.x,
                                         msg.pose.orientation.y,
-                                        msg.pose.orientation.z])
+                                        msg.pose.orientation.z,
+                                        msg.pose.orientation.w])
         
         if not self.receivedPose:
             self.receivedBasePose = True